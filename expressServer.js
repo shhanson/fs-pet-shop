db168bc241e6206683884c44bb02e8f4e7fe5a20
Sarah Hanson <sarahh.hanson@gmail.com>
2017-06-01 15:35:29 -0500

    Completed express server and passed all tests

--- expressServer.js
+++ expressServer.js
@@ -1,3 +1,4 @@
+const bp = require('body-parser');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -6,10 +7,14 @@ const app = express();
 const FILEPATH = path.join(__dirname, 'pets.json');
 const port = 3000;
 
+app.use(bp.json()); // support json encoded bodies
+app.use(bp.urlencoded({
+    extended: true
+})); // support encoded bodies
 
-app.get('/pets', (req,res) => {
+app.get('/pets', (req, res) => {
     fs.readFile(FILEPATH, 'utf8', (err, petData) => {
-        if (err){
+        if (err) {
             console.err(err.stack);
             return res.sendStatus(500);
         }
@@ -22,7 +27,7 @@ app.get('/pets', (req,res) => {
 
 app.get('/pets/:id', (req, res) => {
     fs.readFile(FILEPATH, 'utf8', (err, petData) => {
-        if (err){
+        if (err) {
             console.err(err.stack);
             return res.sendStatus(500);
         }
@@ -30,7 +35,7 @@ app.get('/pets/:id', (req, res) => {
         let petDataObj = JSON.parse(petData);
         let id = parseInt(req.params.id);
 
-        if(id < 0 || id >= petDataObj.length || Number.isNaN(id)){
+        if (id < 0 || id >= petDataObj.length || Number.isNaN(id)) {
             return res.sendStatus(404);
         }
 
@@ -40,11 +45,45 @@ app.get('/pets/:id', (req, res) => {
 
 });
 
-app.use((req,res) => {
-    res.sendStatus(404);
+app.post('/pets', (req, res) => {
+
+    fs.readFile(FILEPATH, 'utf8', (err, petData) => {
+        if (err) {
+            console.err(err.stack);
+            return res.sendStatus(500);
+        }
+
+        let petDataObj = JSON.parse(petData);
+
+        if (req.body.age && !Number.isNaN(req.body.age) && req.body.kind && req.body.name) {
+
+            let pet = {
+                age: Number(req.body.age),
+                kind: req.body.kind,
+                name: req.body.name,
+            };
+
+            petDataObj.push(pet);
+            fs.writeFile(FILEPATH, JSON.stringify(petDataObj), (err) => {
+                if (err) {
+                    console.err(err.stack);
+                    return res.sendStatus(500);
+                }
+            });
+            res.send(pet);
+        } else {
+            res.sendStatus(400);
+        }
+
+    });
 });
 
+app.use((req, res) => {
+    res.sendStatus(404);
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
+
+module.exports = app;
